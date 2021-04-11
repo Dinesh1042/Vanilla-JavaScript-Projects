eecc878066ab213c4782cbbd7b723e8afe816fb2
Dinesh <70156399+Dinesh1042@users.noreply.github.com>
2021-04-11 14:47:34 +0530

    Weather App

---
+++ Weather App/readMe.md
@@ -0,0 +1,11 @@
+# Weather App
+
+![Design]()
+
+The **Weather App** gets your current location and show the current weather of your place with the animation canvas
+
+> #### Note
+
+To use the weather app you must get **API Access Key** from [weatherstack](https://weatherstack.com/) and use you key in the getWeather function
+
+`function getWeather(){ const AccessKey =Enter your access key here; }`
---
+++ Weather App/script.js
@@ -0,0 +1,72 @@
+const countryEl = document.getElementById("country");
+const windSpeedEl = document.getElementById("windSpeed");
+const temperatureEl = document.getElementById("temperature");
+const precipitationEl = document.getElementById("precipitation");
+const formEl = document.getElementById("form");
+
+const iconEl = document.getElementById("icon");
+
+// (function getGoeLocation() {
+//   if (navigator.geolocation)
+//     navigator.geolocation.getCurrentPosition(getUserCityName);
+//   else alert("Geo location is not supported in your browser");
+// })();
+
+async function getUserCityName(position) {
+  const { latitude, longitude } = position.coords;
+  getWeather(`${latitude} , ${longitude}`);
+}
+
+async function getWeather(cityName) {
+  const AccessKey = Null;
+
+  // You must sign in and get your access key in the https://weatherstack.com/
+
+  const weatherApi = `http://api.weatherstack.com/current?access_key=${AccessKey}&query=${cityName}`;
+
+  const fetchData = await fetch(weatherApi);
+  const res = await fetchData.json();
+  updateDOM(res);
+}
+
+function updateDOM(data) {
+  console.log(data);
+  const { region, country } = data.location;
+  const {
+    precip,
+    temperature,
+    wind_speed,
+    weather_descriptions,
+  } = data.current;
+  const icon = weather_descriptions[0].split(" ").join("_").toUpperCase();
+
+  countryEl.innerHTML = `${region}, ${country}`;
+  temperatureEl.innerHTML = temperature;
+  windSpeedEl.innerHTML = wind_speed;
+  precipitationEl.innerHTML = precip;
+
+  setIcon(icon);
+}
+
+function setIcon(value) {
+  let skycons = new Skycons({ color: "#3c5568" });
+  skycons.play();
+  skycons.set(iconEl, Skycons[value]);
+}
+
+formEl.addEventListener("submit", searchWeather);
+
+function searchWeather(e) {
+  e.preventDefault();
+  let userInput = this.querySelector("input");
+  let userLocation = userInput.value;
+  getWeather(userLocation);
+
+  console.log(userLocation);
+
+  userInput.addEventListener("click", (e) => {
+    userInput.select();
+  });
+}
+
+setIcon("SNOW");
